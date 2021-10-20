9d64ff9ff67378e2b672cac1c40e1122942b9e3e
lisali-liang <lisaliliang27@gmail.com>
2021-10-20 11:53:57 -0400

    10/20

--- reactivity/src/App.vue
+++ reactivity/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Product/>
+    <ProductReview />
   </div>
 </template>
 
 <script>
+import Product from './components/Product.vue'
+import ProductReview from './components/ProductReview.vue'
+
 export default {
   name: 'App',
   components: {
+    Product,
+    ProductReview,
   },
   data() {
     return{
---
+++ reactivity/src/components/ProductTabs.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      
+        <ul>
+          <span class="tabs" 
+                :class="{ activeTab: selectedTab === tab }"
+                v-for="(tab, index) in tabs"
+                @click="selectedTab = tab"
+                :key="tab"
+          >{{ tab }}</span>
+        </ul>
+
+        <div v-show="selectedTab === 'Reviews'">
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul v-else>
+                <li v-for="(review, index) in reviews" :key="index">
+                  <p>{{ review.name }}</p>
+                  <p>Rating:{{ review.rating }}</p>
+                  <p>{{ review.review }}</p>
+                </li>
+            </ul>
+        </div>
+
+        <div v-show="selectedTab === 'Make a Review'">
+          <product-review></product-review>
+        </div>
+    
+      </div>
+</template>
+
+<script>
+export default {
+    props: {
+    reviews: {
+      type: Array,
+      required: false,
+    },
+}
+</script>
