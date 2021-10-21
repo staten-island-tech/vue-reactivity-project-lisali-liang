0a114b230765f43f45c65f08b573d213b9cd7cdf
lisali-liang <lisaliliang27@gmail.com>
2021-10-21 12:29:00 -0400

    10/21

--- reactivity/src/App.vue
+++ reactivity/src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app">
     <h1>shopping site</h1>
-    <h1>{{text}}</h1>
+    <h1>{{ text }}</h1>
     <div class="item item-one">
-      
+      <h1>This is an item</h1>
+      <img :src="image" />
+      <p v-if="inventory > 50">In Stock</p>
+      <p v-else>Out of Stock</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
-    
-  },
+  name: "App",
+  components: {},
   data() {
-    return{
-      text:"text"
-    }
-  }
-}
+    return {
+      text: "text",
+      image:
+        "https://images.unsplash.com/photo-1514897575457-c4db467cf78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+      inventory: 100,
+      details: [],
+    };
+  },
+};
 </script>
 
 <style>
